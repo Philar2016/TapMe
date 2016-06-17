bcfcfcec297f823e38357ef459ba6283fa28ac55
LiangMinglong <liangminglong@liangminglongs-macbook-pro.local>
2016-06-17 10:38:43 +0800

    Initial Commit

---
+++ Tap Me/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tap Me
+//
+//  Created by LiangMinglong on 17/06/2016.
+//  Copyright © 2016 LiangMinglong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
